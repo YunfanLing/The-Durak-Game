b306cd58913bcc496ca0e4669acc15ceaabcbeb7
Yunfan Ling <102476857+YunfanLing@users.noreply.github.com>
2022-04-26 00:44:02 +0000

    main version who first

--- .ccls-cache/@home@runner@The-Durak-Game/Trump.h
+++ .ccls-cache/@home@runner@The-Durak-Game/Trump.h
@@ -6,19 +6,70 @@ class Trump
 public:
   void setTrumpCard(Cards AfterShuffle,int cardCount);
   vector<tuple<string,string,int>> getTrumpCard(){return TheTrumpCard;}
-  //int firstTurn(vector<InitialHand>Players);
+  void whoAttackFirst(vector<InitialHand>Players);
+  int hasTrumpSuit(int i,vector<tuple<string,string,int>>mv,string trumpSuit);
 private:
   vector<tuple<string,string,int>>TheTrumpCard;
 };
 
 void Trump::setTrumpCard(Cards AfterShuffle,int cardCount)
 {
-  TheTrumpCard.emplace_back(AfterShuffle.mv[cardCount]);
+  TheTrumpCard.emplace_back(AfterShuffle.mv[35]);
 }
 
-/*int Trump::firstTurn(vector<InitialHand>Players)
+//Who first attack
+//bool hasTrumpSuit(int i,vector<tuple<string,string,int>>mv,string trumpSuit);
+void Trump::whoAttackFirst(vector<InitialHand>Players)
 {
+  string trumpSuit=get<0>(TheTrumpCard[0]);
   int playerName;
-  //If one of the player ha
-  return playerName;
-}*/
+  vector<int>playerHasTrumpSuit;
+  //Check whether Player has trump suit
+  cout<<"Playersize"<<Players.size()<<endl;
+  for(int i;i<Players.size();i++)
+  {
+    //playerHasTrumpSuit.push_back(hasTrumpSuit(i,Players[i].mv,trumpSuit));
+    playerHasTrumpSuit.push_back(1);
+  }
+
+  bool noneTrump; 
+  for(int i;i<playerHasTrumpSuit.size();i++)
+  {
+    cout<<"Player"<<playerHasTrumpSuit[i]<<endl;
+    if(playerHasTrumpSuit[i]==1)
+      noneTrump=0;
+  }  
+    
+  /*If none of the player has trump suit then Player1 start the game
+  if(noneTrump==1)
+  {
+    playerName=1;
+    cout<<"Player"<<playerName<<" start the game\n";
+  }
+  else
+  {
+    vector<tuple<string,string,int>>lowestTrumpCard;
+    lowestTrumpCard[0]={trumpSuit,"A",14};
+    for(int i=0;i<playerHasTrumpSuit.size();i++)
+      if(playerHasTrumpSuit[i]==1)
+      {
+        for(int j=0;j<Players[i].mv.size();j++)
+          if(get<0>(Players[i].mv[j])==trumpSuit)
+            if(get<2>(Players[i].mv[j])<get<2>(lowestTrumpCard[0]))
+            {
+              lowestTrumpCard[0]=Players[i].mv[j];
+              playerName=i;
+            }                     
+      }
+  } */
+  //return playerName;
+}
+
+int Trump::hasTrumpSuit(int i,vector<tuple<string,string,int>>mv,string trumpSuit)
+{
+  int hasTrumpSuit=0;
+  for(int j=0;j<mv.size();j++)
+    if(get<0>(mv[j])==trumpSuit)
+      hasTrumpSuit=1;
+  return hasTrumpSuit;
+}
--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 int playersNumber();
 InitialHand firstDeal(Cards cards,int i);
+
+int hasTrumpSuit(int i,vector<tuple<string,string,int>>mv,string trumpSuit);
+int whoAttackFirst(vector<InitialHand>Players,vector<tuple<string,string,int>>TheTrumpCard);
 int cardCount=0;
+
 int main() 
 {
   Cards AfterShuffle;
@@ -28,11 +32,9 @@ int main()
   Trump TrumpCard;
   TrumpCard.setTrumpCard(AfterShuffle,cardCount);
   cout<<"The trump card is\n";
-  cout<<get<0>(TrumpCard.getTrumpCard()[0])<<' '<<get<1>(TrumpCard.getTrumpCard()[0]);
-  //Initial hand.
-  //for(int i=0;i<Number.playersNumber;i++)
+  cout<<get<0>(TrumpCard.getTrumpCard()[0])<<' '<<get<1>(TrumpCard.getTrumpCard()[0])<<endl;
 
-  //TrumpCard.firstTurn();
+  cout<<whoAttackFirst(Players,TrumpCard.getTrumpCard())<<endl;
   return 0;
 }
 
@@ -58,4 +60,58 @@ InitialHand firstDeal(Cards Cards,int i)
     cardCount++;
   }
   return Result;
+}
+
+//Who first attack
+//bool hasTrumpSuit(int i,vector<tuple<string,string,int>>mv,string trumpSuit);
+int whoAttackFirst(vector<InitialHand>Players,vector<tuple<string,string,int>>TheTrumpCard)
+{
+  string trumpSuit=get<0>(TheTrumpCard[0]);
+  int playerName;
+  vector<int>playerHasTrumpSuit;
+  //Check whether Player has trump suit
+  for(int i=0;i<Players.size();i++)
+  {
+    playerHasTrumpSuit.push_back(hasTrumpSuit(i,Players[i].mv,trumpSuit));
+  }
+  bool noneTrump; 
+  for(int i=0;i<playerHasTrumpSuit.size();i++)
+  {
+    cout<<"Player"<<playerHasTrumpSuit[i]<<endl;
+    if(playerHasTrumpSuit[i]==1)
+      noneTrump=0;
+  }  
+    
+  //If none of the player has trump suit then Player1 start the game
+  if(noneTrump==1)
+  {
+    playerName=1;
+    cout<<"Player"<<playerName<<" start the game\n";
+  }
+  else
+  {
+    vector<tuple<string,string,int>>lowestTrumpCard;
+    lowestTrumpCard.emplace_back(trumpSuit,"A",14);
+    for(int i=0;i<playerHasTrumpSuit.size();i++)
+      if(playerHasTrumpSuit[i]==1)
+      {
+        for(int j=0;j<Players[i].mv.size();j++)
+          if(get<0>(Players[i].mv[j])==trumpSuit)
+            if(get<2>(Players[i].mv[j])<get<2>(lowestTrumpCard[0]))
+            {
+              lowestTrumpCard[0]=Players[i].mv[j];
+              playerName=i+1;
+            }                     
+      }
+  } 
+  return playerName;
+}
+
+int hasTrumpSuit(int i,vector<tuple<string,string,int>>mv,string trumpSuit)
+{
+  int hasTrumpSuit=0;
+  for(int j=0;j<mv.size();j++)
+    if(get<0>(mv[j])==trumpSuit)
+      hasTrumpSuit=1;
+  return hasTrumpSuit;
 }
